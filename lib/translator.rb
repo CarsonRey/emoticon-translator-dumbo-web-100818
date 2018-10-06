942a48ae538fe5b553b9d6f078cf355d5aacb434
Carson Rey <c.rey494@gmail.com>
2018-10-06 19:16:10 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,8 +11,8 @@ def get_japanese_emoticon(emoticons, emo)
   load_library(emoticons)
   if emoticons.include?(emo)
    emoticons["get_meaning"].each do |key, value|
-      if emo == key
-        value
+      if emo == value
+        key
       end
      end
   else
