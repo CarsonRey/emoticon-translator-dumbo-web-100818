fc47f50440d43a17ab021077e4cd8be5582c1b0f
Carson Rey <c.rey494@gmail.com>
2018-10-06 17:19:25 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,6 +10,9 @@ emoticons.each do |word, emoticon_array|
       emoticon_array.each do |emoticon|
         emo_hash[get_meaning] = {emoticon_array[1] => word}
         emo_hash[get_emoticon] = {emoticon_array[0] => emoticon_array[1] }
+      end
+    end
+    
 end
 
 def get_japanese_emoticon(emoticons_path)
