c3d4f61fb4a1ae9389b85540c460927a88f2680b
Carson Rey <c.rey494@gmail.com>
2018-10-06 19:17:54 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,13 +4,14 @@ def load_library(emoticons)
   require 'yaml'
   emoticons = YAML.load_file("./lib/emoticons.yml")
   emoticons
+       binding.pry
 end
 
 def get_japanese_emoticon(emoticons, emo)
   load_library(emoticons)
   if emoticons.include?(emo)
    emoticons["get_meaning"].collect do |key, value|
-     binding.pry
+
       if emo == value
         key
       end
